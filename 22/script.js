fb6165192d378b670b2fb836264d38a5a434317c
Francisco Gimenez Andreis <frangimenez98@gmail.com>
2023-08-04 21:29:46 -0300

    unidad 22 parte 2

--- 22/script.js
+++ 22/script.js
@@ -1,31 +1,16 @@
-// window.addEventListener('load', () => {console.log("cargo")})
-// document.addEventListener('load', () => {console.log("cargo")});
-let errores = document.getElementById("errores");
-let nombre = ''
-function verificarNombre(event) {
-    if(event.target.value != '')
-        nombre = event.target.value
-}
-let apellido = ''
-function verificarApellido(event) {
-    if(event.target.value != '')
-        apellido = event.target.value
-}
-
-let password = ''
-function verificarContraseña(event){
-    if(event.target.value.length <= 8){
-        errores.innerText = "La contraseña, tiene que tener 8 caracteres"
+let form = document.querySelector("form");
+let nombre = document.getElementById("nombre");
+form.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    let valor = nombre.value;
+    let longitud = valor.length;
+    if(longitud < 3){
+        //...
+        console.log("Validacion")
+        nombre.setCustomValidity("El largo minimo es de 3 caracteres")
     }
-    else
-        errores.innerText = ""
-}
-
-let email = ''
-function capturarMail(event){
-    email = event.target.value;
-}
-function registrarme(){
-    let output = document.getElementById("output")
-    output.innerHTML = `<h1>${nombre}</h1><h1>${apellido}</h1><h3>${email}</h3>`;
-}
+    else{
+        nombre.setCustomValidity('')
+    }
+    nombre.reportValidity()
+})
---
+++ 22/testRegex.js
@@ -0,0 +1,5 @@
+const regex = /[A-Z]/g;
+const paragraph = 'The quick brown fox jumps over the lazy dog. It barked.';
+const encontdos = paragraph.match(regex);
+console.log(encontdos);
+console.log(contiene);
