c6066dab3c427c9dfe6e8af63e2dd603e8c72fb8
Francisco Gimenez Andreis <frangimenez98@gmail.com>
2023-09-01 18:46:49 -0300

    add pokemon api

---
+++ ejercicio-pokeapi/app.js
@@ -0,0 +1,40 @@
+function getRandomInt(){
+    return Math.floor(Math.random() * (150 - 1)) + 1;
+}
+document.addEventListener("DOMContentLoaded",()=>{
+    let pokemonId = getRandomInt()
+    fetchData(pokemonId);
+})
+
+function fetchData(id){
+    const http = new XMLHttpRequest();
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`
+    http.open("GET",url)
+    http.send();
+
+    http.onreadystatechange = () => {
+        let data = JSON.parse(http.responseText);
+
+        const pokemon = {
+            nombre: data.name,
+            experiencia: data.base_experience,
+            hp: data.stats[0].base_stat,
+            ataque: data.stats[1].base_stat,
+            defensa: data.stats[2].base_stat,
+            especial:data.stats[3].base_stat,
+            img: data.sprites.other.dream_world.front_default,
+        }
+        mostrarInfo(pokemon)
+    }
+}
+
+function mostrarInfo(pokemon){
+    document.querySelector(".card-body-img").setAttribute("src", pokemon.img)
+    document.querySelector(".card-body-title").innerHTML =`${pokemon.nombre}
+    <span>${pokemon.hp} hp</span>`
+    document.querySelector(".card-body-text").innerText = pokemon.experiencia + ' Exp';
+    document.querySelectorAll(".card-footer-social h3")[0].innerText = pokemon.ataque;
+    document.querySelectorAll(".card-footer-social h3")[1].innerText = pokemon.especial;
+    document.querySelectorAll(".card-footer-social h3")[2].innerText = pokemon.defensa;
+}
+
