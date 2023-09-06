476d2d31c5955b20df2ddd8ba97b10a9f93c9ed4
Francisco Gimenez Andreis <frangimenez98@gmail.com>
2023-09-06 20:28:29 -0300

    añadir ejemplo de carga de datos

--- SPA/app.js
+++ SPA/app.js
@@ -3,6 +3,7 @@ let main = document.querySelector('main')
 links.forEach(link => {
     link.addEventListener("click", () => {
         let archivo = link.id + '.html';
+        history.pushState(null, "", link.id);
         let response = httpRequest(archivo);
         response.addEventListener('load', () => {
             if(response.status === 200){
@@ -12,10 +13,42 @@ links.forEach(link => {
     })
 });
 
+let pagina_inicial = httpRequest('home.html')
+pagina_inicial.addEventListener("load", () => {
+    if(pagina_inicial.status === 200){
+        main.innerHTML = pagina_inicial.responseText;
+    }
+})
+
 function httpRequest(url, metodo){
     let http_metodo = metodo || 'GET';
     let http = new XMLHttpRequest();
     http.open(http_metodo, url)
     http.send()
     return http;
+}
+
+window.addEventListener("popstate",() => {
+    let url = location.pathname.split("SPA/")[1] + '.html';
+    let response = httpRequest(url);
+    response.addEventListener('load', () => {
+        if(response.status === 200){
+            main.innerHTML = response.responseText;
+        }
+    })
+    console.log(url);
+})
+
+function cargarData(){
+    let tableRow = document.getElementById("data");
+    let rawData = httpRequest("people.json");
+    rawData.addEventListener("load", () => {
+        const formatted = JSON.parse(rawData.responseText);
+        formatted.forEach(personaje => {
+            tableRow.innerHTML += `<tr><td>${personaje.name}</td>
+            <td>${personaje.birth_year}</td>
+            <td>${personaje.gender}</td>
+            <td>${personaje.url}</td></tr>`
+        })
+    })
 }
